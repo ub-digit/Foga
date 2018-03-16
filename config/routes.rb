93d32029b7fc75217b1e8914c05a0a6cad14dde0
xgotda <dagmara.gotlib@ub.gu.se>
2018-03-16 14:35:13 +0100

    Started using Ransack bundle. Search on title implemented but not displayed correctly.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   
 
+
+ #  Started using Ransack bundle. Search on title implemented but not displayed correctly.
   #get 'operations/index'
 get 'post_infos/latest'
 
+
+
+  get 'post_infos/search'
 
 resources :post_infos
 resources :operations
@@ -11,12 +16,12 @@ resources :operations
 
 root 'post_infos#index'
 
-# deprecated in v5.2
 #match ':controller(/:action(/:id))', :via => :get
 
 
 
+  #get 'post_infos/index'
 
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # deprecated in v5.2
 end
