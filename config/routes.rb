b93d423f21d4058b3a4aa5e086f4fda89809c61a
xgotda <dagmara.gotlib@ub.gu.se>
2018-04-12 15:08:37 +0200

    Deleted (bad and old) search related code.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   
 
-  resources :searches
   #get 'operations/index'
 
 resources :post_infos
 resources :operations
 
-root 'post_infos#search'
-#'post_infos#index'
+root 'post_infos#index'
 
 # deprecated in v5.2
 #match ':controller(/:action(/:id))', :via => :get
@@ -16,13 +14,7 @@ root 'post_infos#search'
 
 
 
-
-
-
-
-  get 'post_infos/search'
-
-  #get 'post_infos/index'
+ # get 'post_infos/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
