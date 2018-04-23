348333914b21de8ae882eb479bd75feef2da3ceb
xgotda <dagmara.gotlib@ub.gu.se>
2018-04-23 14:45:17 +0200

    Operation (åtgärder) type and description are now limited in terms of length.

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -4,4 +4,8 @@ class Operation < ApplicationRecord
 
 	validates_uniqueness_of :operation_type #, message: 'Denna åtgärd finns redan.'
 
+   validates :description, :length => {:minimum => 0, :maximum => 60}
+
+   validates :operation_type, :length => {:minimum => 0, :maximum => 20}
+
 end
