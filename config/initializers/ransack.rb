6f759616e320150163602b8512d018b4d9904920
xgotda <dagmara.gotlib@ub.gu.se>
2018-05-03 15:31:52 +0200

    Ransack predicate added to extend search to handle space and can use wildcards to some extent.

---
+++ config/initializers/ransack.rb
@@ -0,0 +1,17 @@
+
+Ransack.configure do |config|
+  config.add_predicate 'has_every_term',
+  arel_predicate: 'matches_all',
+  formatter: proc { |v| v.scan(/\"(.*?)\"|(\w+)/).flatten.compact.map{|t| "%#{t}%"} },
+  validator: proc { |v| v.present? },
+  type: :string
+end
+
+
+Ransack.configure do |config|
+  config.add_predicate 'has_any_term',
+  arel_predicate: 'matches_any',
+  formatter: proc { |v| v.scan(/\"(.*?)\"|(\w+)/).flatten.compact.map{|t| "%#{t}%"} },
+  validator: proc { |v| v.present? },
+  type: :string
+end
