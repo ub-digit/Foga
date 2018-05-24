c38ed5c986b9afe390f13118f65e8187b978a3f9
xgotda <dagmara.gotlib@ub.gu.se>
2018-05-24 14:11:06 +0200

    Cleaned up code. Text is in Swedish everywhere.

--- app/models/post_info.rb
+++ app/models/post_info.rb
@@ -6,12 +6,15 @@ class PostInfo < ApplicationRecord
 		:presence => {message: 'Du måste fylla in titel.'},
 		:uniqueness => {message: 'Denna post finns redan.'}
 	
+	validates :operation_id, 
+		:presence => {message: 'Du måste välja en operation.'}
+
+
+	# User validation may be redundant once authentication works.
 	validates :created_by, 
 		:presence => {message: 'Skapad av får inte vara tom.'}
 	
 	validates :updated_by, 
 		:presence => {message: 'Updaterad av får inte vara tom.'}
 
-	validates :operation_id, 
-		:presence => {message: 'Du måste välja en operation.'}
 end
