e1020c50b79f717293a57a25767ff5ecf90095bb
xgotda <dagmara.gotlib@ub.gu.se>
2018-05-24 11:47:21 +0200

    Minor changes to navbar, order of columns and code tidy.

--- app/controllers/post_infos_controller.rb
+++ app/controllers/post_infos_controller.rb
@@ -73,7 +73,7 @@ class PostInfosController < ApplicationController
     end
     @search = PostInfo.search(params[:q])
      
-    @post_infos = @search.result.paginate(:page => params[:page], :per_page => 5)
+    @post_infos = @search.result.paginate(:page => params[:page], :per_page => 8)
   end 
 
 end
