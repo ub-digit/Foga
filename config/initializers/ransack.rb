5b4e18e9601ab9ba65d5aa4df2d9d3356d508f3f
xgotda <dagmara.gotlib@ub.gu.se>
2018-08-24 13:20:15 +0200

    Added functionality to be able to search on åäö and other special signs.

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -2,7 +2,9 @@
 Ransack.configure do |config|
   config.add_predicate 'has_every_term',
   arel_predicate: 'matches_all',
-  formatter: proc { |v| v.scan(/\"(.*?)\"|(\w+)/).flatten.compact.map{|t| "%#{t}%"} },
+  formatter: proc { |v| 
+    v.scan(/\"(.*?)\"|([\-\?\:\'\*\#\+\(\)\{\}\[\]\&\@\=a-zA-Z0-9åäöÅÄÖ]+)/).flatten.compact.map{
+    |t| "%#{t}%"} },
   validator: proc { |v| v.present? },
   type: :string
 end
@@ -14,4 +16,12 @@ Ransack.configure do |config|
   formatter: proc { |v| v.scan(/\"(.*?)\"|(\w+)/).flatten.compact.map{|t| "%#{t}%"} },
   validator: proc { |v| v.present? },
   type: :string
+end
+
+Ransack.configure do |c|
+  c.hide_sort_order_indicators = false
+
+  # Raise errors if a query contains an unknown predicate or attribute.
+  # Default is true (do not raise error on unknown conditions).
+  c.ignore_unknown_conditions = false
 end
