933b265f8ad6d1e90ad161fcbe980b91f550fa3e
xgotda <dagmara.gotlib@ub.gu.se>
2018-08-29 11:19:58 +0200

    Title and publisher not null, plus index on title.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180810124802) do
+ActiveRecord::Schema.define(version: 20180823095657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,16 +31,17 @@ ActiveRecord::Schema.define(version: 20180810124802) do
   end
 
   create_table "post_infos", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.string "issn"
     t.string "created_by", null: false
     t.string "updated_by", null: false
-    t.string "publisher"
+    t.string "publisher", null: false
     t.text "comment"
     t.integer "operation_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "show", default: true
+    t.index ["title"], name: "index_post_infos_on_title"
   end
 
   create_table "users", force: :cascade do |t|
