61714feed1dccacaf50809861c4fb0cc37523b7f
xgotda <dagmara.gotlib@ub.gu.se>
2018-08-31 12:18:54 +0200

    Minor code reshuffling.

--- app/controllers/post_infos_controller.rb
+++ app/controllers/post_infos_controller.rb
@@ -61,13 +61,12 @@ class PostInfosController < ApplicationController
       params[:q].permit!
       params[:q][:show_true] = "1"  #Only show posts that aren't "deleted"
     end
-
-    #look at @search for åäö problem. SQL done when???
+  
     @search = PostInfo.search(params[:q])
     @search.sorts = 'title asc' if @search.sorts.empty?
-    # Default value is chosen if none is selected (in case params[:aper_page]=nil).
-    show_per_page = params[:aper_page] || 50
+    show_per_page = params[:aper_page] || 50    # Default value is chosen if none is selected.
     @post_infos = @search.result.paginate(per_page: show_per_page, page: params[:page])
+
   end 
 
 
